97a63147cfdd03a697265996c36173849cc6a055
Mike Kenway <mike.kenway@wegolook.com>
2026-02-12 19:44:09 -0500

    fixed broken logging imports

--- logging/logging.go
+++ logging/logging.go
@@ -3,30 +3,28 @@ package logging
 import (
 	"fmt"
 	"log"
-
-	"github.com/ekkolyth/go-tools/config"
 )
 
 func Fatal(message string, err error) {
-	log.Fatal(config.CLI_RED + "[FATAL]: " + config.CLI_RESET + message + err.Error() + "\n\n")
+	log.Fatal(CLI_RED + "[FATAL]: " + CLI_RESET + message + err.Error() + "\n\n")
 }
 
 func Info(message string, args ...any) {
 	output := fmt.Sprintf(message, args...)
-	log.Println(config.CLI_BLUE + "[INFO]: " + config.CLI_RESET + output)
+	log.Println(CLI_BLUE + "[INFO]: " + CLI_RESET + output)
 }
 
 func Warning(message string, args ...any) {
 	output := fmt.Sprintf(message, args...)
-	log.Println(config.CLI_YELLOW + "[WARN]: " + config.CLI_RESET + output)
+	log.Println(CLI_YELLOW + "[WARN]: " + CLI_RESET + output)
 }
 
 func Api(message string, args ...any) {
 	output := fmt.Sprintf(message, args...)
-	log.Println(config.CLI_BRIGHT_CYAN + "[API]: " + config.CLI_RESET + output)
+	log.Println(CLI_BRIGHT_CYAN + "[API]: " + CLI_RESET + output)
 }
 
 func Worker(message string, args ...any) {
 	output := fmt.Sprintf(message, args...)
-	log.Println(config.CLI_BRIGHT_MAGENTA + "[WORKER]: " + config.CLI_RESET + output)
+	log.Println(CLI_BRIGHT_MAGENTA + "[WORKER]: " + CLI_RESET + output)
 }
